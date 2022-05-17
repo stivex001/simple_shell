f5be9a33b985caf653555bccc68bf6bc1a428e14
Lurldgbodex <professionaleditor6@gmail.com>
2022-05-17 06:12:45 -0700

    task 1 complete

--- main.c
+++ main.c
@@ -4,24 +4,32 @@
 
 /**
  * main - prints a simple shell
+ * @ac: number of arguments
+ * @argv: arrays of arguments
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int ac, char **argv)
 {
-	char cmdline[MAXLINE];
+	(void)ac;
+	char *prog_name = argv[0];
+	char command[MAXLINE];
+
 
 	while (1)
 	{
 		printf("$ ");
-		if ((fgets(cmdline, MAXLINE, stdin) == NULL) && ferror(stdin))
+		if ((fgets(command, MAXLINE, stdin) == NULL) && ferror(stdin))
 			perror("fgets error");
 		if (feof(stdin))
 		{
 			printf("\n");
 			exit(0);
 		}
-		//eval(cmdline);
+		if (command != "exit")
+			printf("%s: No such file or directory\n", prog_name);
+		else
+			break;
 	}
 	return (0);
 }
